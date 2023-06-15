9f91091294d59fbac821dba00d63f61f67948e04
ChandraKarthik07 <99110756+ChandraKarthik07@users.noreply.github.com>
2023-06-15 22:52:12 +0530

    2

--- metacomic_1/settings.py
+++ metacomic_1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-kuve**!%o=xua)p1mwe=wv&gk7=_%u%yaw8ax66&67@bcl1bsb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -138,7 +138,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 
 # Default primary key field type
