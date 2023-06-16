7d3cd502a4055aa14c02c7ce52dbf98faa71722d
ChandraKarthik07 <99110756+ChandraKarthik07@users.noreply.github.com>
2023-06-16 11:49:06 +0530

    7

--- manage.py
+++ manage.py
@@ -1,22 +1,21 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metacomic_1.settings')
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metacomic_1.settings")
     try:
         from django.core.management import execute_from_command_line
+        from uvicorn import run
+
+        # Run Django with Uvicorn
+        if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
+            run("metacomic_1.asgi:application", host="0.0.0.0", port=8000)
+        else:
+            execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
